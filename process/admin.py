63f2369a1581acac16003ce342e83c782925c9a3
Ailson Azevedo <ailsonsilvaazevedo1@gmail.com>
2022-10-04 14:13:29 -0300

    feat: add model process and parts

--- process/admin.py
+++ process/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
-from .models import Process
+from .models import Process, Parts
 
-admin.site.register(Process)
+@admin.register(Process)
+class ProcessAdmin(admin.ModelAdmin):
+    list_display = ('number', 'judicialClass', 'topic', 'judge', 'category', 'created', 'modified')
+    list_filter = ('number', 'judicialClass', 'judge', 'category')
+    search_fields = ('number', 'judicialClass', 'judge', 'category')
 
-# Register your models here.
+@admin.register(Parts)
+class PartsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created', 'modified')
+    list_filter = ('name',)
+    search_fields = ('name',)   
--- process/models.py
+++ process/models.py
@@ -7,8 +7,29 @@ class Base(models.Model):
     class Meta:
         abstract = True
 
+class Parts(Base):
+    name = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Part'
+        verbose_name_plural = 'Parts'
+
 class Process(Base):
     number = models.CharField(max_length=1000)
-    grade = models.CharField(max_length=1000)
-    
-# Create your models here.
+    judicialClass = models.CharField(max_length=500)
+    topic = models.CharField(max_length=1000)
+    judge = models.CharField(max_length=500)
+    parts = models.ManyToManyField(Parts)
+    category = models.CharField(max_length=500, null=False)
+
+    def __str__(self):
+        return self.number
+
+    class Meta:
+        ordering = ['-created']
+        verbose_name = 'Process'
+        verbose_name_plural = 'Processes'
+
